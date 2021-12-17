3f0a132a972a47410cb4a1ef003326279a4ca034
Ermir Suldashi <ermirsuldashi@gmail.com>
2021-12-17 04:46:55 +0100

    added acceleration example in c6

--- c6/src/index.js
+++ c6/src/index.js
@@ -1,10 +1,11 @@
+import * as PIXI from 'pixi.js'
 import * as MainLoop from 'mainloop.js';
 
-let xVelocity = 1;
-let yVelocity = 1;
+let xVelocity = 2;
+let yVelocity = 0;
 
 let xPosition = 60;
-let yPosition = 120;
+let yPosition = 0;
 
 const gameWidth = 640;
 const gameHeight = 360;
@@ -17,33 +18,36 @@ const app = new PIXI.Application({ width: gameWidth, height: gameHeight });
 document.getElementById("pixi-root").appendChild(app.view);
 
 const obj = new PIXI.Graphics();
+const gravity = 0.2;
 app.stage.addChild(obj);
 MainLoop.setUpdate(() => {
     obj.clear();
     obj.beginFill(rectColor);
     obj.drawRect(xPosition, yPosition, rectWidth, rectHeight);
+    yVelocity+=gravity;
     xPosition += xVelocity;
     yPosition += yVelocity;
-    if(xPosition + rectWidth === gameWidth) {
-        xVelocity = -1;
+    if(xPosition + rectWidth >= gameWidth) {
+        let diff = xPosition + rectWidth - gameWidth;
+        xPosition -= diff;
+        xVelocity = -xVelocity;
         rectColor = randomColor();
     }
-    if(xPosition === 0) {
-        xVelocity = 1;
+    if(xPosition <= 0) {
+        let diff = xPosition;
+        xPosition+=diff;
+        xVelocity = -xVelocity;
         rectColor = randomColor();
     }
-    if(yPosition + rectHeight === gameHeight) {
-        yVelocity = -1;
-        rectColor = randomColor();
-    }
-    if(yPosition === 0) {
-        yVelocity = 1;
+    if(yPosition + rectHeight > gameHeight) {
+        let diff = yPosition + rectHeight - gameHeight;
+        yPosition = gameHeight - rectHeight - diff;
+        yVelocity = -yVelocity;
         rectColor = randomColor();
     }
 });
 
 MainLoop.start();
-
 
 function randomColor() {
     let red = Math.floor(Math.random()*256);
